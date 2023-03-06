464f176b6126c20e72c8f71e1150a749fec25430
Serafim840 <57246485+Serafim840@users.noreply.github.com>
2023-03-06 08:09:36 +0300

    Добавление техники

--- warehouses/webapp/urls.py
+++ warehouses/webapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('item/<int:item_id>/', views.item, name="item info"),
+    path('item/create/', views.create_item, name="create item"),
     path('warehouse/<int:warehouse_id>/', views.warehouse_all, name="warehouse info"),
     path('warehouse/<int:warehouse_id>/count', views.warehouse_count, name="warehouse count")
+
 ]
--- warehouses/webapp/views.py
+++ warehouses/webapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Count
+from django.urls import reverse
 from .models import Item, Warehouse, ItemInWarehouse
 
 def index(request):
@@ -9,15 +10,32 @@ def index(request):
     return render(request, "index.html", {'warehouses': warehouses, 'items': items})
 
 def item(request, item_id):
-    return HttpResponse(str(Item.objects.get(id=item_id)))
+    result = get_object_or_404(Item, pk=item_id)
+    return HttpResponse(str(result))
+
+def create_item(request):
+    if request.method == 'GET':
+        return render(request, "create_item.html")
+    elif request.method == 'POST':
+        try:
+            new_item = Item.objects.create(
+                brand=request.POST['brand'],
+                country=request.POST['country'],
+                model=request.POST['model']
+            )
+        except KeyError:
+            return render(request, "create_item.html")
+        else:
+            new_item.save()
+            return HttpResponseRedirect(reverse('item info', args=(new_item.pk,)))
 
 def warehouse_all(request, warehouse_id):
-    warehouse = Warehouse.objects.get(id=warehouse_id)
+    warehouse = get_object_or_404(Warehouse, pk=warehouse_id)
     results = ItemInWarehouse.objects.filter(warehouse_id=warehouse_id).select_related('item')
     return render(request, "warehouse.html", {'warehouse': warehouse, 'results': results, 'count': False})
 
 def warehouse_count(request, warehouse_id):
-    warehouse = Warehouse.objects.get(id=warehouse_id)
+    warehouse = get_object_or_404(Warehouse, pk=warehouse_id)
     results = ItemInWarehouse.objects.filter(warehouse_id=warehouse_id).values('item', 'item__country', 'item__brand', 'item__model').annotate(item_count=Count('item'))
     return render(request, "warehouse.html", {'warehouse': warehouse, 'results': results, 'count': True})
 
